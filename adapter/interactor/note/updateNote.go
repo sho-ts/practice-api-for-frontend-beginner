8fc92b8a822d7192820c895adc2b6791f55c3be9
tagawa shouta <sho.webfe@gmail.com>
2022-11-25 02:22:32 +0900

    ファイル再生成

--- adapter/interactor/note/updateNote.go
+++ adapter/interactor/note/updateNote.go
@@ -1,12 +1,15 @@
 package interactor
+
 import (
   "note-app/domain/dto/input/note"
   "note-app/domain/dto/output/note"
 )
+
 type updateNoteInteractor struct{}
 func NewUpdateNoteInteractor() updateNoteInteractor {
   return updateNoteInteractor{}
 }
+
 func (i updateNoteInteractor) Handle(in input.UpdateNoteInput) output.UpdateNoteOutput {
   
 }
